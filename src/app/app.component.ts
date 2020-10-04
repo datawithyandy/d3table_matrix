f2a4b5718fc14e2e06a8abfaee97cb0a017d4e53
datawithyandy <yandy.plasencia@datahavenanalytics.com>
2020-10-04 11:35:54 -0400

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, VERSION } from "@angular/core";
+import { csv, extent, interpolateGreens, merge, mouse, scaleBand, scaleSequential, select, Selection, text } from 'd3';
+import { Corpus, Similarity } from 'tiny-tfidf';
+
 
 @Component({
   selector: "my-app",
@@ -17,5 +20,20 @@ export class AppComponent implements OnInit {
     this.setState();
   }
 
-  async setState() {}
+  async setState() {
+    let matrix: any,
+      tooltip: any,
+      corpus: Corpus,
+      color: any,
+      extendedMatrix: Array<any>,
+      div: Selection<any, any, HTMLElement, any> | any,
+      K1 = 2.0,
+      b = 0.75,
+      useStopwords = true,
+      customStopwords = [],
+      maxCF = 30,
+      similarity: Similarity,
+      size = 850,
+      margin = { topAndLeft: 120, bottomAndRight: 120 };
+  }
 }
