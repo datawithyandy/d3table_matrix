0fea2e93847e60131eefaa3b5f8f79856f6cf17c
datawithyandy <yandy.plasencia@datahavenanalytics.com>
2020-10-04 11:35:12 -0400

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  metadata: Array<any> | any;
+  states: Array<string> | any;
+  files: Array<string> | any;
+  speeches: Array<string> | any;
+
+  name = "Angular " + VERSION.major;
+
+  ngOnInit() {
+    this.setState();
+  }
+
+  async setState() {}
 }
